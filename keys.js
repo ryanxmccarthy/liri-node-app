e04501d008f63695695fcdc1b7c621e4b1842ea8
Ryan McCarthy <ryanxmccarthy@gmail.com>
2017-10-27 11:49:46 -0400

    initial files and keys

---
+++ keys.js
@@ -0,0 +1,10 @@
+console.log('this is loaded');
+
+var twitterKeys = {
+  consumer_key: 'EEDml0ELLQr8375Xw7xlToFsT',
+  consumer_secret: 'W3ktFUN6LPWYClIVIItHYq0d2rAeenVd5CMSXX1AqJE5j9zu4u',
+  access_token_key: '2790092151-zDI2OIepj0jeLSEpzUfqjRo5IAsHFBRT6oeZNNq',
+  access_token_secret: 'KUClCI34y0xbC0KLnmTh3rVCtXefbYnGqZuBgtfhBNPGe',
+}
+
+module.exports = twitterKeys;
