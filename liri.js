072985b7b5b21a260e88120e14eea3d5855ee4bc
Ryan McCarthy <ryanxmccarthy@gmail.com>
2017-10-29 17:58:25 -0400

    added spotify

--- liri.js
+++ liri.js
@@ -2,7 +2,14 @@ var inquirer = require("inquirer");
 
 var Twitter = require('twitter');
 
+var Spotify = require('node-spotify-api');
+
 var keys = require('./keys.js');
+
+var spotify = new Spotify({
+  id: 'efee065879d74d72925470820cd414e3',
+  secret: '8ee79d2c2fc1410ab416b16b3a95f8b4',
+});
 
 var client = new Twitter({
   consumer_key: 'EEDml0ELLQr8375Xw7xlToFsT',
@@ -11,26 +18,33 @@ var client = new Twitter({
   access_token_secret: 'KUClCI34y0xbC0KLnmTh3rVCtXefbYnGqZuBgtfhBNPGe',
 }); 
 
-inquirer.prompt([
-	{
-      type: "list",
-      message: "Choose a method",
-      choices: ['my-tweets', 'spotify-this-song', 'movie-this', 'do-what-it-says'],
-      name: "method"
-    },
-]).then(function(response) {
-	if (response.method === 'my-tweets') {
-		var params = {screen_name: 'yungtechgod'};
-		client.get('statuses/user_timeline', params, function(error, tweets, response) {
-		  if (!error) {
-		    console.log(tweets);
-		  }
+var arg = process.argv;
+
+var command = arg[2];
+
+var name = arg[3];
+
+if (command === 'my-tweets') {
+	var params = {
+		screen_name: 'yungtechgod'
+	};
+	client.get('statuses/user_timeline', params, function(error, tweets, response) {
+	  if (!error) {
+	    console.log(tweets);
+	  }
+	});
+} else if (command === 'spotify-this-song') {
+	spotify.request('https://api.spotify.com/v1/tracks/7yCPwWs66K8Ba5lFuU2bcx')
+		.then(function(data) {
+	    	console.log(data.album.artists[0].name);
+	    	console.log(data.name);
+	    	console.log(data.preview_url);
+	    	console.log(data.album.name); 
+	  	}).catch(function(err) {
+	    	console.error('Error occurred: ' + err); 
 		});
-	} else if (response.method === 'spotify-this-song') {
-		console.log('here is the song')
-	} else if (response.method === 'movie-this') {
-		console.log('here is the movie')
-	} else if (response.method === 'do-what-it-says') {
-		console.log('im doing what you said')
-	}
-})
+} else if (command === 'movie-this') {
+	console.log('here is the movie')
+} else if (command === 'do-what-it-says') {
+	console.log('im doing what you said')
+}
